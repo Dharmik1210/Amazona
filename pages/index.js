e70e97d72f1470e435e45e7ec044c1ff7e609e3e
Dharmik1210 <dhpatel1210@gmail.com>
2022-08-10 19:02:01 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 //import Head from 'next/head';
+import data from '../utils/data';
 import Layout from '../components/Layout';
+import Productitem from '../components/Productitem';
 // import Image from 'next/image';
 //import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return <Layout title="home page">Home page</Layout>;
+  return (
+    <Layout title="home page">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <Productitem product={product} key={product.slug}></Productitem>
+        ))}
+      </div>
+    </Layout>
+  );
 }
